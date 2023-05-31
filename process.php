30fa0bcadaee716d01390abfb4e9741dff94972a
sfantaye <sfantaye@outlook.com>
2023-05-31 16:30:19 +0300

    address modified

--- process.php
+++ process.php
@@ -2,7 +2,7 @@
 $servername = "localhost";
 $username = "root";
 $password = "";
-$dbname = "appointments";
+$dbname = "addisappointer";
 
 // Create a connection
 $conn = new mysqli($servername, $username, $password, $dbname);
