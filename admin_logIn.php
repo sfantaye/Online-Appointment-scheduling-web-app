d7e9a4b3448f65d9524dc5c5509fbc7799b51c47
sfantaye <sfantaye@outlook.com>
2023-06-12 21:15:04 +0300

    APIs completed

---
+++ admin_logIn.php
@@ -0,0 +1,53 @@
+<?php
+
+session_start();
+
+
+$hostname = 'localhost';
+$username = 'root';
+$password = '';
+$database = 'addisappointer';
+
+$conn = new mysqli($hostname, $username, $password, $database);
+
+if ($conn->connect_error) {
+    die('Connection failed: ' . $conn->connect_error);
+}
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $username = sanitizeInput($_POST['username']);
+    $password = sanitizeInput($_POST['password']);
+
+  
+    $stmt = $conn->prepare('SELECT username, password, name FROM admin_account WHERE username = ? LIMIT 1');
+    $stmt->bind_param('s', $username);
+    $stmt->execute();
+    $stmt->bind_result($storedUsername, $storedPassword, $name);
+    $stmt->fetch();
+
+    if ($storedUsername === $username && $password === $storedPassword) {
+        
+        $_SESSION['logged_in'] = true;
+        $_SESSION['username'] = $username;
+        $_SESSION['name'] = $name;
+
+        
+        header('Location: view-company-appointments.php');
+        exit();
+    } else {
+       
+        echo 'Invalid username or password.';
+    }
+
+    $stmt->close();
+}
+
+$conn->close();
+
+function sanitizeInput($input) {
+   
+    $input = trim($input);
+    $input = stripslashes($input);
+    $input = htmlspecialchars($input);
+    return $input;
+}
+?>
---
+++ view-company-appointments.php
@@ -0,0 +1,140 @@
+<style>
+    table {
+        
+        border-collapse: collapse;
+        width: 100%;
+    }
+    .container, .container1{
+        background-color:gray;
+        margin:50px;
+        padding:40px
+      
+       
+    }
+   
+    h1{
+        text-align:center;
+        font-family: verdana;
+        font-size:2em;
+
+    }
+
+    th, td {
+        padding: 8px;
+        text-align: left;
+        border-bottom: 1px solid #ddd;
+    }
+
+    th {
+        background-color: #f2f2f2;
+    }
+
+    tr:hover {
+        background-color: #f5f5f5;
+    }
+</style>
+
+
+<?php
+session_start();
+
+if (!isset($_SESSION['username'])) {
+    header('Location: admin_logIn.html');
+    exit();
+}
+
+$username = $_SESSION['username'];
+
+
+
+$hostname = 'localhost';
+$dbUsername = 'root';
+$password = '';
+$database = 'addisappointer';
+
+$conn = new mysqli($hostname, $dbUsername, $password, $database);
+
+if ($conn->connect_error) {
+    die('Connection failed: ' . $conn->connect_error);
+}
+
+
+$currentDateTime = date('Y-m-d H:i:s');
+
+$stmt1 = $conn->prepare("SELECT name FROM admin_account WHERE username = '$username'");
+$stmt1->execute();
+$result1 = $stmt1->get_result();
+$row = $result1->fetch_assoc();
+$name = $row['name'];
+
+
+$stmt = $conn->prepare('SELECT a.id, a.name, a.email, a.phone, a.date, a.time, a.service, a.comments, a.timestamp 
+                       FROM appointment a
+                       JOIN users u ON a.email = u.email
+                       JOIN admin_account d ON a.service = d.name
+                       WHERE  d.name = ?
+                         AND (a.date > CURDATE() OR (a.date = CURDATE() AND a.time > CURTIME()))');
+
+$stmt->bind_param("s", $name);
+$stmt->execute();
+$result = $stmt->get_result();
+echo '<h1>Your company has the following appointments</h1>';
+echo '<hr>';
+echo"<div class='container'";
+echo '<h1>Future Appointments</h1>';
+echo '<table border>';
+echo '<tr><th>ID</th><th>Name</th><th>Email</th><th>Phone</th><th>Date</th><th>Time</th><th>Service</th><th>Comments</th><th>Timestamp</th></tr>';
+while ($row = $result->fetch_assoc()) {
+    echo '<tr>';
+    echo '<td>' . $row['id'] . '</td>';
+    echo '<td>' . $row['name'] . '</td>';
+    echo '<td>' . $row['email'] . '</td>';
+    echo '<td>' . $row['phone'] . '</td>';
+    echo '<td>' . $row['date'] . '</td>';
+    echo '<td>' . $row['time'] . '</td>';
+    echo '<td>' . $row['service'] . '</td>';
+    echo '<td>' . $row['comments'] . '</td>';
+    echo '<td>' . $row['timestamp'] . '</td>';
+    echo '</tr>';
+}
+echo '</table>';
+echo "</div>";
+
+$stmt->close();
+
+$stmt = $conn->prepare('SELECT a.id, a.name, a.email, a.phone, a.date, a.time, a.service, a.comments, a.timestamp 
+                       FROM appointment a
+                       JOIN users u ON a.email = u.email
+                       JOIN admin_account d ON a.service = d.name
+                       WHERE  d.name = ?
+                         AND (a.date < CURDATE() OR (a.date = CURDATE() AND a.time < CURTIME()))');
+
+$stmt->bind_param("s", $name);
+
+$stmt->execute();
+$result = $stmt->get_result();
+echo "<div class='container1'";
+echo '<h1>Past Appointments</h1>';
+echo '<table border>';
+echo '<tr><th>ID</th><th>Name</th><th>Email</th><th>Phone</th><th>Date</th><th>Time</th><th>Service</th><th>Comments</th><th>Timestamp</th></tr>';
+while ($row = $result->fetch_assoc()) {
+    echo '<tr>';
+    echo '<td>' . $row['id'] . '</td>';
+    echo '<td>' . $row['name'] . '</td>';
+    echo '<td>' . $row['email'] . '</td>';
+    echo '<td>' . $row['phone'] . '</td>';
+    echo '<td>' . $row['date'] . '</td>';
+    echo '<td>' . $row['time'] . '</td>';
+    echo '<td>' . $row['service'] . '</td>';
+    echo '<td>' . $row['comments'] . '</td>';
+    echo '<td>' . $row['timestamp'] . '</td>';
+    echo '</tr>';
+}
+echo '</table>';
+echo "</div>";
+
+$stmt->close();
+
+$conn->close();
+?>
+
